8e9aa7198f87cb9e997d5c1d462b6c128e525281
Mustafa Ihab <mustafaihab9@gmail.com>
2025-09-23 15:08:22 +0300

    adding axios-instance

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,1 +1,34 @@
-// axios
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// add token if it available
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("auth_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized, redirecting to login...");
+      window.location.href = "/auth/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
